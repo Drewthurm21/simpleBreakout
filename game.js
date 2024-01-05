f17c972af963130700102ae6b085f4d71f657327
Drewthurm21 <drewthurm21@gmail.com>
2024-01-05 13:07:42 -0600

    added ball collision with walls, decoupled ball rendering and position update, set new intervals

--- game.js
+++ game.js
@@ -3,14 +3,16 @@ const canvas = document.getElementById('gameCanvas');
 const context = canvas.getContext('2d');
 
 //ball data
-let pos_x = 0;
-let pos_y = 0;
+let pos_x = 20;
+let pos_y = 20;
+let dir_x = 5;
+let dir_y = 5;
 let ballSize = 10;
-let dir_x = 1;
-let dir_y = 1;
+let ballSpeed = 60;
 
 
-const createBall = () => {
+const drawBall = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
   context.beginPath();
   context.arc(pos_x, pos_y, ballSize, 0, 2 * Math.PI);
   context.fillStyle = '#00FF00'
@@ -19,15 +21,14 @@ const createBall = () => {
 }
 
 const updateBallPosition = () => {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  pos_x += dir_x;
+  pos_y += dir_y;
 
-  createBall();
-
-  pos_x += ballSize + dir_x;
-  pos_y += ballSize + dir_y;
+  if (pos_x + dir_x > canvas.width - ballSize || pos_x < ballSize) dir_x = -dir_x;
+  if (pos_y + dir_y > canvas.height - ballSize || pos_y < ballSize) dir_y = -dir_y;
 }
 
-let interval = setInterval(() => {
-  console.log('interval')
+let ballDrawInterval = setInterval(drawBall, 5);
+let ballSpeedInterval = setInterval(() => {
   updateBallPosition();
-}, 100);
+}, 100 - ballSpeed);
